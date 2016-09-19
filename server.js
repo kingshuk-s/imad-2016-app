8e2ca501880049e2d938cd0bf69fb8bf498d739d
kingshuk-sagar <kingshuk.sagar@gmail.com>
2016-09-19 13:21:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.get('/', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
-app.get('/article-one', function(req,res) {
- res.send("this is article one");
+app.get('/ui/article-one', function(req,res) {
+ res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
