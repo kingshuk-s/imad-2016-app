c39d03c9a06b9138a98459f02cfb0cf732b76a01
kingshuk-sagar <kingshuk.sagar@gmail.com>
2016-10-22 21:40:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,7 @@ console.log('Loaded!');
 //var element = document.getElementById('main-text');
 //element.innerHTML="new value";
 var img = document.getElementById('me');
-marginright=0;
+var marginright=0;
 function moveleft(){
     marginright= marginright + 10;
     img.style.marginright= marginright + 'px' ;
