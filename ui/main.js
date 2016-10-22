537d1029d357d7d622317703caac13b1cb36c023
kingshuk-sagar <kingshuk.sagar@gmail.com>
2016-10-22 20:18:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,8 @@
 console.log('Loaded!');
 var element = document.getElementById('main-text');
 element.innerHTML="new value";
+var img = document.getElementById('me');
+img.onclick=function(){
+    var interval= setInterval(moveleft,200);
+    img.style.marginright='200px';
+};
